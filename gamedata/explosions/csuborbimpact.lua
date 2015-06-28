b09d445994fe49e5e6d5335a33ad38676e4d411f
PicassoCT <PicassoCT@gmx.de>
2015-06-28 23:37:04 +0200

    new file:   gamedata/explosions/csuborbimpact.lua 	new file:   objects3d/cichneumondea.s3o

---
+++ gamedata/explosions/csuborbimpact.lua
@@ -0,0 +1,29 @@
+return {
+  ["csuborbimpact"] = {
+  
+ 
+								bitmapmuzzleflame = {
+								  air                = true,
+								  class              = [[CBitmapMuzzleFlame]],
+								  count              = 1,
+								  ground             = true,
+								  underwater         = 1,
+								  water              = true,
+								  properties = {
+									colormap           = [[1 0.55 0.9 0.005	  1 0.55 0.9 0.004	1 0.3 0.55 0.003  1 0.3 0.55 0.001	]],
+									dir                = [[0,1,0]],
+									frontoffset        = 150,
+									fronttexture       = [[bluedot]],
+									length             = 500,
+									sidetexture        = [[TouchGround]],
+									size               = 150,
+									sizegrowth         = 0.01,
+									ttl                = 5,
+								  },
+								},
+							   
+							  
+
+				},
+}
+
