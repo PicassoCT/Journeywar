9af717b7dbf99b21352201d3377f5a17a5bad6f2
PicassoCT <PicassoCT@gmx.de>
2015-11-17 00:00:18 +0100

    deleted:    LuaRules/Gadgets/dbg_BehaviourProfiling.luad 	deleted:    LuaUI/widgets/dbg_chili_inspector.luad 	deleted:    LuaUI/widgets/dbg_chili_profiler.luad 	deleted:    LuaUI/widgets/tp_buttonsender.luad 	deleted:    sounds/music/LoadingSound.ogg

--- LuaRules/Gadgets/jw_SpawnerAI.lua
+++ LuaRules/Gadgets/jw_SpawnerAI.lua
@@ -1,7 +1,9 @@
 -- In-game, type /luarules kpai in the console to toggle the ai debug messages
  
 function gadget:GetInfo()
-local modOptions = Spring.GetModOptions()
+
+	
+	
         return {
                 name = "Spawner AI",
                 desc = "Spawns Units",
@@ -9,11 +11,82 @@ local modOptions = Spring.GetModOptions()
                 date = "around since last tuesday",
                 license = "GPOCNL - Gajop Pissed Off Cause Nonsense License ",
                 layer = 82,
-              enabled = (modOptions.SpawnerAI and modOptions.SpawnerAI== true) or false
+              enabled = true
         }
 end
 
+
+
 if (gadgetHandler:IsSyncedCode()) then
+	gaiaTeam=Spring.GetGaiaTeamID()
+	teamTable={}
+	meridianTable={}
+	spawnUnits={bio={"jgeohive", "jbeehive"}
+				,tec={"bonker","sentry","coverworldgate","combinedfeature"}
+				}
+	function spawnSpawners(frame)
+		
+		volume=math.sin(frame/10000)*15
+		
+		boolBio= volume < 0
+		volume=math.abs(volume)
+		
+		for i=1, volume do
+			meridian=meridianTable[math.random(1,#meridianTable)]
+			percent=math.random(0,1)
+			px=math.abs(percent*(meridian.start.x)- (1-percent)*(meridian.lend.x))
+			pz=math.abs(percent*(meridian.start.z)- (1-percent)*(meridian.lend.z))
+			
+				if boolBio==true then	
+					Spring.CreateUnit(spawnUnits.bio[math.random(1,#spawnUnits.bio)],px,0,pz,0,gaiaTeam)
+				else                                                                      
+					Spring.CreateUnit(spawnUnits.tec[math.random(1,#spawnUnits.tec)],px,0,pz,0,gaiaTeam)	
+				end		
+		end
+	end
+	
+	
+	spawnerAI={}
+	function gadget:Initialize()
+		boolAtLeastOneSPawner=false
+	for _, t in ipairs (Spring.GetTeamList ()) do
+	
+        local teamID, leader, isDead, isAI, side = Spring.GetTeamInfo (t)
+        if isAI and Spring.GetTeamLuaAI (teamID) == "Spawner AI" then
+            spawnerAI[teamID] = true
+        
+			boolAtLeastOneSPawner=true
+        else
+		px,py,pz= Spring.GetTeamStartPosition(t)
+		if px then
+			teamTable[t]={x=px,z=pz}
+		end
+		end
+    end
+    if boolAtLeastOneSPawner == false then
+        gadgetHandler:RemoveGadget ()
+    end
+		for team, tab in ipairs (teamTable) do
+			for ateam, atab in ipairs (teamTable) do
+				if team ~=ateam then
+					meridianTable[#meridianTable]={start=tab,lend=atab}
+				end
+			end
+		end
+	
+	end
+
+	end
+	
+	incRate=0
+	total=9000-incRate
+	function gadget:GameFrame(frame)
+	if frame > 0 and frame % total == 0 then
+		spawnSpawners(frame)
+		incRate=math.abs(math.sin(frame/1000)*1024)
+		total=9000-incRate
+	end
+	end
 
 
 end
