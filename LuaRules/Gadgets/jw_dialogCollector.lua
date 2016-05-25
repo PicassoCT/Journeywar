a2a18fdf8e71fee4f30816af000a31e1cb5ee9b8
florian.seidl-schulz <Florian.Seidl-Schulz@sai-schweiger.de>
2016-05-25 19:04:27 +0200

    unfied the dialog drawing widget with the corresponding gadget

--- LuaRules/Gadgets/jw_dialogCollector.lua
+++ LuaRules/Gadgets/jw_dialogCollector.lua
@@ -1,9 +1,9 @@
 
 function gadget:GetInfo()
 	return {
-		name = "on Fi",
-		desc = "Kills the infected ",
-		author = "zwzsg",
+		name = "Dialog",
+		desc = "Draws DialogLines of Units ",
+		author = "PicassoCT",
 		date = "3rd of May 2010",
 		license = "Free",
 		layer = 0,
@@ -12,6 +12,9 @@ function gadget:GetInfo()
 	}
 end
 
+----------------------------------------------------------
+
+	
 -- modified the script: only corpses with the customParam "featuredecaytime" will disappear
 
 if (gadgetHandler:IsSyncedCode()) then
@@ -20,12 +23,36 @@ if (gadgetHandler:IsSyncedCode()) then
 			if frame % 5== 0 and GG.Dialog then
 
 			itterator=#dialog
-				for k,v in pairs(_G.Dialog) do	
+				for k,v in pairs(GG.Dialog) do	
 					if v.frames <= 0 then 
 					GG.Dialog[k]= nil 
 					end
 				end
+				--Send to Unsynced
+				_G.Dialog = GG.Dialog
 			end
-	
 		end
+else --UNSYNCED
+
+local glText = gl.Text
+
+
+	function widget:DrawScreenEffects()		
+		if SYNCED and SYNCED.Dialog then
+
+			for _,id in ipairs(Spring.GetAllUnits()) do	
+				if SYNCED.Dialog[id] then 
+				local dialog = _G.Dialog[id]			
+				x,y,z =Spring.GetUnitPosition(id)
+				local FontSize=12
+				for i, elements in pairs(dialog) do		
+					--offset =((elements.y/elements.frames)*elements.y)+(#dialog)*i*FontSize
+					local x,y=Spring.WorldToScreenCoords(Spring.GetUnitPosition(id))
+					glText(elements.line,x,y,FontSize,"od")
+				end
+				end
+			end
+		end
+	end		
 end
+
