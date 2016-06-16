be4ab3b8f3adfdf2c56fa91cfe3734ad59fae1c0
PicassoCT <PicassoCT@gmx.de>
2016-06-16 22:47:59 +0200

    adapte the jantarts

--- gamedata/explosions/jantseverywhere.lua
+++ gamedata/explosions/jantseverywhere.lua
@@ -12,14 +12,15 @@
       properties = {
         airdrag            = 1,
          colormap           = [[0.25 0.20 0.10 1.0	0 0 0 0.0]],
+
         directional        = true,
         emitrot            = 0,
         emitrotspread      = 90,
         emitvector         = [[0,1,0]],
         gravity            = [[0, 0.00000007, 0]],
         numparticles       = 320,
-        particlelife       = 150,
-        particlelifespread = 60,
+        particlelife       = 70,
+        particlelifespread = 15,
         particlesize       = 0.7,
         particlesizespread = 0.2,
         particlespeed      = 0.002,
@@ -31,8 +32,39 @@
         useairlos          = false,
       },
     },
-		
-	  
+	
+	spark = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      water              = true,
+      properties = {
+        airdrag            = 0.8,
+        alwaysvisible      = true,
+      colormap           = [[0.8 1 0.2 0.025		0.8 1 0.2 0.0125]],
+        directional        = false,
+        emitrot            = 0,
+        emitrotspread      = 0,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, 0.005, 0]],
+        numparticles       = 1,
+        particlelife       = 55,
+        particlelifespread = 8,
+        particlesize       = 15,
+        particlesizespread = 0,
+        particlespeed      =0,
+        particlespeedspread = 0,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = 0.8,
+        sizemod            = 1.000001,
+        texture            = [[laserend]],
+        useairlos          = false,
+      },
+    },
+	
+	
+	 
 		
 	  
     },
--- gamedata/explosions/jlessants.lua
+++ gamedata/explosions/jlessants.lua
@@ -29,7 +29,49 @@
         useairlos          = false,
       },
     },
-		
+	-- stripe = {
+    
+
+      -- air                = true,
+      -- class              = [[CBitmapMuzzleFlame]],
+      -- count              = 1,
+      -- ground             = true,
+      -- underwater         = false,
+      -- water              = true,
+      -- properties = {
+        -- colormap           = [[0.8 1 0.2 0.01  	0.8 1 0.2 0.01 		0.8 1 0.2 0.01  	0.8 1 0.2 0.01 	]],
+        -- dir                = [[dir]],
+        -- frontoffset        = 0,
+        -- fronttexture       = [[empty]],
+        -- length             = -12,
+        -- sidetexture        = [[citdronegrad]],
+        -- size               = -3,
+        -- sizegrowth         = 0.18,
+        -- ttl                = 24,
+      -- }
+    -- },	
+
+	stripe = {
+    
+
+      air                = true,
+      class              = [[CBitmapMuzzleFlame]],
+      count              = 1,
+      ground             = true,
+      underwater         = false,
+      water              = true,
+      properties = {
+        colormap           = [[0.8 1 0.2 0.01  	0.8 1 0.2 0.01 		0.8 1 0.2 0.01  	0.8 1 0.2 0.01 	]],
+        dir                = [[dir]],
+        frontoffset        = 0,
+        fronttexture       = [[empty]],
+        length             = -12,
+        sidetexture        = [[citdronegrad]],
+        size               = -6,
+        sizegrowth         = 0,
+        ttl                = 94,
+      }
+    },	
 		
 	  
 				 
