f8b9d2831e3a6ed566c54f4b09608948ea83633a
PicassoCT <PicassoCT@gmx.de>
2016-09-21 15:18:55 +0200

    Version {58.1} Reduce Testcase

--- scripts/dbg_iktestscript.lua
+++ scripts/dbg_iktestscript.lua
@@ -77,11 +77,12 @@ function iktest()
 		-- ikz= math.ceil( math.random(-50,50))
 		-- iky= math.ceil( math.random(0,50))
 		-- ux,uy,uz=Spring.GetUnitPosition(unitID)
-		for i=1, # testArray do
-		Spring.Echo("Setting IK-Goal(X:"..testArray[i].x.."/Z:"..testArray[i].z)
-		StartThread(markPosOnMap,testArray[i].x, testArray[i].y, testArray[i].z,"greenlight")		
+		--for i=1, # testArray do
+		--Spring.Echo("Setting IK-Goal(X:"..testArray[i].x.."/Z:"..testArray[i].z)
+		i=1
+		StartThread(markPosOnMap,testArray[i].x, testArray[i].y+10, testArray[i].z,"greenlight")		
 		Spring.SetUnitIKGoal(unitID,ikID,testArray[i].x,0,testArray[i].z)
-		Sleep(10000)
+		Sleep(3000)
 		
 		end
 	end
