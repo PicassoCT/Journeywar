c3a8c8fbdc514614f4352c87c8453d63e0b63460
Picasso <PicassoCT@gmx.de>
2016-10-03 21:13:58 +0200

    Chanes

---
+++ units/journey/jhunter.lua
@@ -0,0 +1,58 @@
+local unitName = "hunter"
+
+local unitDef = {
+name = "hu ter",
+Description = "Small Tripod",
+objectName = "jHunter.s3o",
+script = "jhunterscript.lua",
+buildPic = "placeholder.png",
+--cost
+buildCostMetal = 2500,
+buildCostEnergy = 1200,
+buildTime = 50,
+--Health
+maxDamage = 10500,
+idleAutoHeal = 2,
+--Movement
+Acceleration = 0.25,
+BrakeRate = 0.3,
+FootprintX = 5,
+FootprintZ = 5,
+MaxSlope = 80,
+MaxVelocity = 1.4,
+MaxWaterDepth = 65,
+MovementClass = "Default2x2",
+TurnRate = 250,
+
+sightDistance = 900,
+upright=true,
+onoffable=true,
+activateWhenBuilt =false,
+CanAttack = true,
+CanGuard = true,
+CanMove = true,
+CanPatrol = true,
+CanStop = true,
+LeaveTracks = true, 
+
+Builder = false,
+ShowNanoSpray = true,
+CanBeAssisted = false,
+CanReclaim=false,	
+
+	explodeAs = [[NOWEAPON]],
+	selfDestructAs= [[NOWEAPON]], 
+ 
+ 
+Category = [[LAND]],
+
+weapons = {
+[1]={name  = "jdartgun",
+	onlyTargetCategory = [[LAND]],
+	mainDir=[[0 0 1]],--nerfed
+	maxAngleDif        = 90,--nerfed
+	},
+},
+}
+
+return lowerkeys({ [unitName] = unitDef })
---
+++ weapons/jdartgun.lua
@@ -0,0 +1,58 @@
+local weaponName="jdartgun"
+local weaponDef={
+	name="jdartgu ",
+	weaponType=[[MissileLauncher]],
+	
+	Accuracy=2000,
+	
+	--Physic/flight path
+	range=800,
+	reloadtime=8,
+	weaponVelocity=1000,
+	startVelocity=150,
+	weaponAcceleration=250,
+	flightTime=2.5,
+	BurnBlow=1,
+	FixedLauncher=false,
+	dance=0,--1
+	wobble=0,--4
+	
+	tolerance=16000,
+	tracks=false,
+	Turnrate=32000,
+	collideFriendly=false,
+	
+	----APPEARANCE
+	model="jhunterdart.s3o",
+	smokeTrail=false,
+	--explosionGenerator="custom:redsmoke",
+	CegTag="jbirdtrail",
+	
+	----TARGETING
+	turret=true,
+	cylinderTargeting=66,
+	avoidFeature=true,
+	avoidFriendly=true,
+	
+	
+	--commandfire=true,
+	
+	----DAMAGE
+	damage={
+		default=150,
+		heavyarmor=150,
+	},
+	areaOfEffect=100,
+	craterMult=0,
+	
+	--?FIXME***
+	lineOfSight=true,
+	
+	
+	--sound
+--	soundHit="sounds/skinfantry/birdrockethit.ogg",
+--	soundStart="sounds/skinfantry/birdrocketstart.ogg",
+	
+}
+
+return lowerkeys ({[weaponName]=weaponDef})
