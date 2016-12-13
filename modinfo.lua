232d51b58eb0683441b9818778a96c221333df03
PepeAmpere <machys@gmail.com>
2016-12-13 03:40:45 +0100

    First beta release ready for notAlab distribution [https://trello.com/c/1mw4qQVY/]

--- modinfo.lua
+++ modinfo.lua
@@ -5,7 +5,7 @@ local modinfo = {
 	shortgame = "JW",
 	description = "Nature vs Technology - This game is not suitable for minors or children. ",
 	url = "http://springrts.com/phpbb/viewtopic.php?f=69&t=23275",
-	version = '$VERSION',
+	version = 'beta-161213',
 	modtype = "1",
 	--Perfectionism is the death of good things
 	depend = {
