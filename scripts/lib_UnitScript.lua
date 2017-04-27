42326e4c52b753955d408b01ed44cc81259daeea
PicassoCT <PicassoCT@gmx.de>
2017-04-27 21:39:13 +0200

    so much change for so little money

--- scripts/lib_UnitScript.lua
+++ scripts/lib_UnitScript.lua
@@ -27,6 +27,18 @@ lib_boolDebug= false --GG.BoolDebug or false
 --======================================================================================
 -->make a GlobalTableHierarchy From a Set of Arguments - String= Tables, Numbers= Params
 -->Example: "TableContaining[key].TableReamining[key].valueName" or [nr] , value
+ function split(div,str)
+  if (div=='') then return false end
+  local pos,arr = 0,{}
+  -- for each divider found
+  for st,sp in function() return string.find(str,div,pos,true) end do
+    table.insert(arr,string.sub(str,pos,st-1)) -- Attach chars left of current divider
+    pos = sp + 1 -- Jump past current divider
+  end
+  table.insert(arr,string.sub(str,pos)) -- Attach chars right of last divider
+  return arr
+end
+
 function makeTableFromString(FormatString,assignedValue, ...)
 	local arg = arg ; if (not arg) then arg = {...}; arg.n = #arg end
 	if loadstring(FormatString) ~= nil then FormatString=FormatString.."="..assignedValue; loadstring(FormatString) return end
