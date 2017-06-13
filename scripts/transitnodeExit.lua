ca11cbd0ecf8f3ecb9cac41043a67ca11764f45c
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2017-06-13 14:16:14 +0200

    fixed the digital Shown Digital Numbers

--- scripts/transitnodeExit.lua
+++ scripts/transitnodeExit.lua
@@ -6,6 +6,43 @@ Low1 = piece "Low1"
 Low2 = piece "Low2"
 
 
+LED = {}
+for i = 1, 14 do
+    LED[#LED + 1] = {}
+
+    piecename = "LED" .. i
+
+    LED[#LED] = piece(piecename)
+    Hide(LED[i])
+end
+
+DecoderArray = {}
+
+
+DecoderArray[0] = {	1, 	1	,1	,1	,1	,1	,0}
+DecoderArray[1] = {	0, 	1 	,1 	,0 	,0 	,0 	,0}
+DecoderArray[2] = {	1, 	1 	,0 	,1 	,1 	,0 	,1}
+DecoderArray[3] = {	1, 	1 	,1 	,1 	,0 	,0 	,1}
+DecoderArray[4] = {	0, 	1 	,1 	,0 	,0 	,1 	,1}
+DecoderArray[5] = {	1, 	0 	,1 	,1 	,0 	,1 	,1}
+DecoderArray[6] = {	1, 	0 	,1 	,1 	,1 	,1 	,1}
+DecoderArray[7] = {	1, 	1 	,1 	,0 	,0 	,0 	,0}
+DecoderArray[8] = {	1, 	1 	,1 	,1 	,1 	,1 	,1}
+DecoderArray[9] = {	1, 	1 	,1 	,1 	,0 	,1 	,1}
+
+
+function binary(num)
+    -- returns a table of bits, least significant first.
+    local t = {} -- will contain the bits
+    while num > 0 do
+        rest = math.fmod(num, 2)
+        t[#t + 1] = rest
+        num = (num - rest) / 2
+    end
+    return t
+end
+
+
 function littleCarsGoingBackAndForth()
     while true do
         Move(Low1, z_axis, 120, 45)
@@ -32,6 +69,26 @@ function sound()
     end
 end
 
+function display7DigitNumber(number)
+
+    firstDigit = number % 11
+    if firstDigit == 10 then firstDigit = 0 end
+    secondDigit = math.floor(number / 10)
+
+
+    tx = DecoderArray[firstDigit]
+    ty = DecoderArray[secondDigit]
+
+    for i = 1, 7, 1 do
+        if tx[i] == 0 then Hide(LED[i]) else Show(LED[i]) end
+    end
+
+    for i = 8, 14, 1 do
+        if ty[i] == 0 then Hide(LED[i]) else Show(LED[i]) end
+    end
+end
+
+
 function script.Create()
     StartThread(sound)
     StartThread(littleCarsGoingBackAndForth)
@@ -39,6 +96,7 @@ function script.Create()
     Spin(Wheel1, z_axis, math.rad(-70), 0)
     Spin(Wheel2, z_axis, math.rad(70), 0)
     --StartThread(main)
+	display7DigitNumber(GG.TransitHubNumber[unitID] or 0)
 end
 
 function script.Killed(recentDamage, _)
