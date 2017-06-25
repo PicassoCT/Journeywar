2928469c73c606fa2a0321a7f2267c6baa0b8646
PicassoCT <PicassoCT@gmx.de>
2017-06-25 11:49:57 +0200

    Various Fixes
    Gadget still needs to be fixed - so it is timewise distributed spawning units

--- LuaRules/Gadgets/dbg_unitTestGadget.lua
+++ LuaRules/Gadgets/dbg_unitTestGadget.lua
@@ -6,25 +6,13 @@ function gadget:GetInfo()
 		date = "SPAM/SPAM/SPAM/LOVELYSPAM",
 		version = "v0.1.ham.and.jam.and...",
 		license = "GPL spam v3.0 or later",
-		layer = -1,
+		layer = math.huge,
+		handler = true,
 		enabled = true
 	}
 end
 
-local function startCustomUnitTest()
-	tID = Spring.GetMyTeamID()
-	Spring.SendLuaRulesMsg("TCT|" .. tID)
-	Spring.Echo("Unittest starts")
-end
-
-local function startGenericUnitTest()
-	tID = Spring.GetMyTeamID()
-	Spring.SendLuaRulesMsg("TGT|" .. tID)
-	Spring.Echo("Unittest starts")
-end
-
-if gadgetHandler:IsSyncedCode() then
-	
+if gadgetHandler:IsSyncedCode() then	
 	-------------------------------------
 	-- Includes
 	VFS.Include('scripts/lib_UnitScript.lua')
@@ -33,17 +21,12 @@ if gadgetHandler:IsSyncedCode() then
 	--Every Tests consists of a preparation function, called once and returning the handed over persistancepackage
 	--It also consists of a testFunction, which returns the testCompletness and the persistancepackage
 	Tests={}
-	function gadget:Initialize()
-		gadgetHandler.actionHandler.AddChatAction(gadget,"unittest custom", startCustomUnitTest, "")
-		gadgetHandler.actionHandler.AddChatAction(gadget,"unittest generic", startGenericUnitTest, "")
-	end
-	
 	-- CONFIG --
 
 	function gadget:RecvLuaMsg(msg, playerID)
 		if msg then
 			ident = string.lower(string.sub(msg, 1, 3))
-			if ident 
+			if ident then
 				testmode= "generic"
 				if ident == "tct" then testmode ="custom" end
 				playerTeamID = tonumber(string.sub(msg, 5, string.len(msg)))
@@ -64,26 +47,26 @@ if gadgetHandler:IsSyncedCode() then
 		
 		mapSizeX, mapSizeZ = Game.mapSizeX / (pow2 / 2), Game.mapSizeZ / (pow2 / 2)
 		x, z = 1, 1
-		teamID = playerTeamID 
-		if 
+		teamID = playerTeamID or Spring.GetGaiaTeamID()
+
 		
 		for k, v in pairs(UnitDefNames) do
-		GenericUnitTests[#GenericUnitTests+1] ={}
+		GenericUnitTests[#GenericUnitTests+1] ={} 
 			x = ringcrement(x, mapSizeX, 1)
 			if x == mapSizeX then
 				z = ringcrement(z, mapSizeZ, 1)
 			end
 		
 		GenericUnitTests[#GenericUnitTests].preparation= function (persPackage)
-			id = Spring.CreateUnit(v.id, x, 0, z, teamID)
+			id = Spring.CreateUnit(v.id, x, 0, z, 1, teamID)
 			Command(id, "stop", {}, { "shift" })
-			Command(id, "cloak", {}, { "shift" })
-			Command(id, "cloak", {}, { "shift" })
-			Command(id, "move", { x = x, y = 0, z = zunit + 25 }, { "shift" })
-			Command(id, "setactive", {}, { "shift" })
-			Command(id, "setactive", {}, { "shift" })
+			-- Command(id, "cloak", {}, { "shift" })
+			-- Command(id, "cloak", {}, { "shift" })
+			Command(id, "move", { x = x, y = 0, z = z + 25 }, { "shift" })
+			-- Command(id, "setactive", {}, { "shift" })
+			-- Command(id, "setactive", {}, { "shift" })
 			Command(id, "attack", { x = x, y = 0, z = z + 25 }, { "shift" })
-			Command(id, "setactive", {}, { "shift" })
+			-- Command(id, "setactive", {}, { "shift" })
 			Command(id, "attack", { x = x, y = 0, z = z - 25 }, { "shift" })
 			persPackage.id = id
 			return persPackage
@@ -94,13 +77,10 @@ if gadgetHandler:IsSyncedCode() then
                 --only apply if Unit is still alive
                 if Spring.GetUnitIsDead(persPack.id) == true then
                     return nil, persPack
-                end
-
-       
+                end      
 
                 return frame + 50, persPack
-            end
-			
+            end		
 		end
 		
 		return GenericUnitTests
@@ -114,8 +94,6 @@ if gadgetHandler:IsSyncedCode() then
          GG.EventStream:CreateEvent(Test.fullfilled,
              persPackage,
              Spring.GetGameFrame() + 1)
-                 
-                 
 		end
 	end
 	
@@ -127,4 +105,21 @@ if gadgetHandler:IsSyncedCode() then
 		executeTests(Tests)
 	end
 	
+else
+	local function startCustomUnitTest()
+		tID = Spring.GetMyTeamID()
+		Spring.SendLuaRulesMsg("TCT|" .. tID)
+		Spring.Echo("Unittest custom starts")
+	end
+
+	local function startGenericUnitTest()
+		tID = Spring.GetMyTeamID()
+		Spring.SendLuaRulesMsg("TGT|" .. tID)
+		Spring.Echo("Unittest generic starts")
+	end
+
+	function gadget:Initialize()
+		gadgetHandler.actionHandler.AddChatAction(gadget,'utest_cust', startCustomUnitTest,  "starts a generic unittest")
+		gadgetHandler.actionHandler.AddChatAction(gadget,'utest_gen', startGenericUnitTest, "starts the users specified tests")
+	end
 end
