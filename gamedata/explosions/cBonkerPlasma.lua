29fa6bd5ac858974708361ddf63b2250e42b564e
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2017-07-02 15:09:01 +0200

    removed unused tags from cegs

--- gamedata/explosions/cBonkerPlasma.lua
+++ gamedata/explosions/cBonkerPlasma.lua
@@ -27,17 +27,14 @@ return {
       water              = true,
       properties = {
           colormap           = [[0.6 1 1 0.0045  0.6 1 1 0.0045   0.5 0.9 1 0.0045    0.1 0.05 0.8 0.0045  ]],
-	  --  colormap           = [[0 1 0.5 0.03    0 0 0 0.01]],
+
         dir                = [[dir]],
         frontoffset        = -50,
         fronttexture       = [[]],
         length             = 450,
-		emitrot            = 0,
-        emitrotspread      = 0,
         sidetexture        = [[pulseshot]],
-	    particlesizespread = 1.2,
         size               = 15.5,
-		sizemod            = 1.0,
+
         sizegrowth         = 0,
         ttl                = 15,
       },
