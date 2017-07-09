ab48a92206cb3e7c73530e89caf72a169b1b874f
Picasso <PicassoCT@gmx.de>
2017-07-09 21:42:47 +0200

    refactored test

--- LuaRules/Gadgets/dbg_unitTestGadget.lua
+++ LuaRules/Gadgets/dbg_unitTestGadget.lua
@@ -8,7 +8,7 @@ function gadget:GetInfo()
 		license = "GPL spam v3.0 or later",
 		layer = math.huge,
 		handler = true,
-		enabled = false
+		enabled = true
 	}
 end
 
@@ -24,21 +24,7 @@ if gadgetHandler:IsSyncedCode() then
 	-- CONFIG --
 	boolUnitTestInProgress= false
 	testedUnitTypeIDs= {}
-	function gadget:RecvLuaMsg(msg, playerID)
-		if msg then
-			ident = string.lower(string.sub(msg, 1, 3))
-			if ident then
-				testmode= "generic"
-				if ident == "tct" then testmode ="custom" end
-					if ident == "tct" or ident == "tgt" then
-						playerTeamID = tonumber(string.sub(msg, 5, string.len(msg)))
-						boolUnitTestInProgress= true
-						testedUnitTypeIDs = {}
-		
-					end
-			end
-		end
-	end
+
 	
 	function getNonAIAkaPlayer(List)
 		for i=1,#List do
@@ -50,13 +36,25 @@ if gadgetHandler:IsSyncedCode() then
 	return List[math.random(1,#List)]
 	end
 	
-	function gadget:GameFrame(n)
-		if n > 1 and n % 900 == 0 then		
+	UnitTest={}
+	function initialize()
 			teamList = Spring.GetTeamList()			
-			unitTest(getNonAIAkaPlayer(teamList), "generic")	
-		end
+			UnitTest= generateGenericTests()
+	
 	end
 	
+	function gadget:GameFrame(n)
+		if n == 1 and n % 900 == 0 then		
+			initialize()
+		end
+		
+		if UnitTest[n] then
+			UnitTest[n].startTest( UnitTest[n].defID,  UnitTest[n].pos)
+		end
+		
+	end
+	
+	frameOffset= 0
 	function generateGenericTests()
 
 	UnitsTotal = table.getn(UnitDefNames)
@@ -72,72 +70,31 @@ if gadgetHandler:IsSyncedCode() then
 
 		
 		for k, v in pairs(UnitDefNames) do
-		GenericUnitTests[#GenericUnitTests+1] ={persPackage = {id = v.id}} 
+			frameOffset =frameOffset+50
+			frame = Spring.GetGameFrame() + 200 + frameOffset
+		
+
 			x = ringcrement(x, mapSizeX, 1)
 			if x == mapSizeX then
 				z = ringcrement(z, mapSizeZ, 1)
 			end
-		
-		persPackage.x,persPackage.z = x,z
-		
-		GenericUnitTests[#GenericUnitTests].preparation = function (persPackage)
-				id = Spring.CreateUnit(persPackage.id, x, 0, z, 1, teamID)
+			
+				GenericUnitTests[frame] ={defID  = v.id, pos = {x=x, z= z }, startTest = function (defID, pos)
+				id = Spring.CreateUnit(defID, pos.x, 0, pos.z, 1, teamID)
 				Command(id, "stop", {}, { "shift" })
-				Command(id, "move", { x = persPackage.x, y = 0, z = persPackage.z + 25 }, { "shift" })
-				Command(id, "attack", { x = persPackage.x, y = 0, z = persPackage.z + 25 }, { "shift" })
-				Command(id, "move", { x = persPackage.x, y = 0, z = persPackage.z - 25 }, { "shift" })
-				Command(id, "attack", { x = persPackage.x, y = 0, z = persPackage.z - 25 }, { "shift" })
-				persPackage.unitID = id
-			return persPackage
-			end
-		
-			GenericUnitTests[#GenericUnitTests].fullfilled =    function(evtID, frame, persPack, startFrame)
-                --Spring.Echo("EventStream Poisoned by active for"..persPack.victimID)
-                --only apply if Unit is still alive
-                if Spring.GetUnitIsDead(persPackage.unitID) == true then
-                    return nil, persPack
-                end      
+				Command(id, "move", { x = pos.x, y = 0, z = pos.z + 25 }, { "shift" })
+				Command(id, "attack", { x = pos.x, y = 0, z = pos.z + 25 }, { "shift" })
+				Command(id, "move", { x = pos.x, y = 0, z = pos.z - 25 }, { "shift" })
+				Command(id, "attack", { x = pos.x, y = 0, z = pos.z - 25 }, { "shift" })
+				return id
 
-                return frame + 50, persPack
-            end		
+			end}
 		end
 		
 		return GenericUnitTests
 	end
 	
-	function executeTests(Tets)
-		for num, Test in pairs(Tests) do
-			persPackage= Test.preparation(Test.persPackage)			
 
-			GG.EventStream:CreateEvent(Test.fullfilled,
-             persPackage,
-             Spring.GetGameFrame() + num * 5)
-		end
-	end
 	
-	function unitTest(playerTeamID, mode)
-		if mode == "generic" then
-			Tests= generateGenericTests()
-		end
-		
-		executeTests(Tests)
-	end
-	
-else
-	local function startCustomUnitTest()
-		tID = Spring.GetMyTeamID()
-		Spring.SendLuaRulesMsg("TCT|" .. tID)
-		Spring.Echo("Unittest custom starts")
-	end
 
-	local function startGenericUnitTest()
-		tID = Spring.GetMyTeamID()
-		Spring.SendLuaRulesMsg("TGT|" .. tID)
-		Spring.Echo("Unittest generic starts")
-	end
-
-	function gadget:Initialize()
-		gadgetHandler.actionHandler.AddChatAction(gadget,'utest_cust', startCustomUnitTest,  "starts a generic unittest")
-		gadgetHandler.actionHandler.AddChatAction(gadget,'utest_gen', startGenericUnitTest, "starts the users specified tests")
-	end
 end
