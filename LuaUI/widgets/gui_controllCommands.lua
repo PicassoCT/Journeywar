a30fd85761b29a2d1699eea451bb18e9da4d51bd
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2017-07-18 16:13:23 +0200

    Moved gui_helper into /gui_lib/

--- LuaUI/widgets/gui_controllCommands.lua
+++ LuaUI/widgets/gui_controllCommands.lua
@@ -30,7 +30,7 @@ local controllCommand_window
 local activeCommand = 0
 
 VFS.Include("LuaUI/widgets/guiEnums.lua")
-VFS.Include("LuaUI/widgets/gui_helper.lua")
+VFS.Include("LuaUI/widgets/gui_lib/gui_helper.lua")
 ---------------------------------------------------------------------------------------
 
 
