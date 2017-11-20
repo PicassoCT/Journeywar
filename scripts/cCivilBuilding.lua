db66581c47582f588e0b09a973848b6ad774c589
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2017-11-20 13:18:47 +0100

    Extracted jw-specific elements from the UnitScript Lib

--- scripts/cCivilBuilding.lua
+++ scripts/cCivilBuilding.lua
@@ -3,6 +3,7 @@ include "lib_OS.lua"
 include "lib_UnitScript.lua"
 include "lib_Animation.lua"
 
+include "lib_jw.lua"
 include "lib_Build.lua"
 
 --HitByWeapon ( x, z, weaponDefID, damage ) -> nil | number newDamage 
