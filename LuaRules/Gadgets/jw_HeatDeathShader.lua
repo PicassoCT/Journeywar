d1ffd1e42a0139e2d8c01b85307f54f04c85cbf6
Florian Seidl -Schulz <PicassoCT@gmx.de>
2017-12-10 23:20:42 +0100

    modified glowworm, deactivated heat death shader

--- LuaRules/Gadgets/jw_HeatDeathShader.lua
+++ LuaRules/Gadgets/jw_HeatDeathShader.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
         date = "Sep. 20014",
         license = "GPL 3.141",
         layer = 256,
-        enabled = true, --until the problem of all units having the shader applied is fixed
+        enabled = false, --until the problem of all units having the shader applied is fixed
     }
 end
 
