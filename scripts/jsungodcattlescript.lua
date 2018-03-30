d2067962ecf25557a88f7043f4ca49be7e8225c1
Picasso <PicassoCT@gmx.de>
2018-03-30 15:35:23 +0200

    added helios cattle blood effect

--- scripts/jsungodcattlescript.lua
+++ scripts/jsungodcattlescript.lua
@@ -83,6 +83,7 @@ function layDownYourWorryHead()
     Signal(SIG_GET)
 	legDown(8)
 
+    Turn(Main, z_axis, math.rad(0), 12)
     Turn(turnerPoint, z_axis, math.rad(-45), 12)
     WaitForTurn(turnerPoint, z_axis)
 
@@ -118,6 +119,7 @@ function getUpAndMove()
 	legDown(80)
 	WaitForTurns(LegTable)
     Turn(turnerPoint, z_axis, math.rad(0), 0.27)
+    Turn(Main, z_axis, math.rad(zRotationMain), 0.27)
 standUpFactor=1
 steps=400
     while (true == Spring.UnitScript.IsInTurn(piecesTable[2], z_axis))  do
@@ -168,11 +170,11 @@ steps=400
 		 		 
 		for i = 1, #LegTable do
 			if i % 2 == 0 then
-				tSyncIn(LegTable[i], -25+ math.random(-5,5),0, 0,800)
-				tSyncIn(LLegTable[i],25+ math.random(-5,5),0, 0,800)			
+				tSyncIn(LegTable[i], -25+ math.random(-5,5),0, -zRotationMain,800)
+				tSyncIn(LLegTable[i],25+ math.random(-5,5),0, -zRotationMain,800)			
 			else
-				tSyncIn(LegTable[i],30 +math.random(-5,5),0, 0,800)
-				tSyncIn(LLegTable[i],-30 + math.random(-5,5),0, 0,800)
+				tSyncIn(LegTable[i],30 +math.random(-5,5),0,-zRotationMain,800)
+				tSyncIn(LLegTable[i],-30 + math.random(-5,5),0, -zRotationMain,800)
 			end			
         end  	
 		Sleep(800)	
@@ -221,8 +223,7 @@ end
 
 function legDown(speed)
 	speed=speed or 3.25
-	
-	resetT(LegTable, speed)
+	turnT(LegTable,0,0,-zRotationMain,speed)
 end
 
 function script.StopMoving()
@@ -242,8 +243,40 @@ oldFrame = 0
 currentFrame = 0
 cegSpawnValue = 225
 boolFirstTime=false
+boolCurrentlyDrawing=false
+function drawBlood(x,z)
+if boolCurrentlyDrawing== true then return end
+boolCurrentlyDrawing= true
+raddeg= (x/z)*2*math.pi
+firstBlood= TableOfPieceGroups["Blood"]
+turnPieceRandDir(firstBlood,0, 360,-360,360,-360,360,-360)
+Turn(firstBlood,y_axis,math.rad(raddeg),0)
+spinT(TableOfPieceGroups["Blood"],z_axis,5,-42,42)
+process(TableOfPieceGroups["Blood"],
+		function(id)
+			movePieceToPiece(unitID,id, firstBlood, 0)
+			WaitForMoves(id)
+			spinRand(id, -22,22, 0.02)
+			mSyncIn(id,0,0,0,1500)
+		
+		end
+		)
+		Sleep(1500)
+process(TableOfPieceGroups["Blood"],
+		function(id)
+			if id ~= firstBlood then
+				movePieceToPiece(unitID,id, firstBlood,3)		
+			end
+		end
+		)		
+		WaitForMoves(TableOfPieceGroups["Blood"])
+boolCurrentlyDrawing=false
+end
 
 function script.HitByWeapon(x, z, weaponDefID, damage)
+	if damage > 50 then
+		StartThread(drawBlood,x,z)
+	end
 
     if damage then
         boolDefBuffActive = false
@@ -377,15 +410,36 @@ function threadStarter()
 end
 
 TableOfPieceGroups={}
+zRotationMain=math.random(0,50)
+function buildUnit()
+
+	for i=1, #TableOfPieceGroups["rRock"] do
+		indexPiece = TableOfPieceGroups["rRock"][i]
+		if maRa() then Show(indexPiece) end
+		turnPieceRandDir(indexPiece,0, 360,-360,360,-360,360,-360)
+	end
+
+end
+function testDrawBlood()
+	while true do
+		x,z= math.random(-100,100)/100,math.random(-100,100)/100
+		drawBlood(x,z)
+		Sleep(1000)
+	end
+end
 function script.Create()
 	Spring.SetUnitBlocking(unitID,false)
 	resetAll(unitID)
 	setSpeedEnv(unitID,1.0)
 	TableOfPieceGroups = getPieceTableByNameGroups(false, true)
+	StartThread(testDrawBlood)
 	hideT( TableOfPieceGroups["Egg"])
 	LegTable= TableOfPieceGroups["Leg"]
 	LLegTable= TableOfPieceGroups["LLeg"]
+	hideT(TableOfPieceGroups["Blood"])
+	hideT(TableOfPieceGroups["rRock"])
 	hideT(TableOfPieceGroups["Egg"])
+	buildUnit()
     if not GG.SunGodCattleTable then GG.SunGodCattleTable = {} end
     if not GG.SunGodCattleTable[myTeamID] then GG.SunGodCattleTable[myTeamID] = 0 end
     GG.SunGodCattleTable[myTeamID] = GG.SunGodCattleTable[myTeamID] + 1
@@ -401,7 +455,6 @@ function script.QueryWeapon1()
 end
 
 function spreadLegs(counterPitch,speed)
-	
 	Turn(TableOfPieceGroups["Leg"][1],z_axis,counterPitch+math.rad(60),speed)
 	Turn(TableOfPieceGroups["LLeg"][1],z_axis,math.rad(-60),speed)	
 	Turn(TableOfPieceGroups["Leg"][3],z_axis,counterPitch+math.rad(60),speed)
@@ -458,7 +511,9 @@ function script.AimWeapon1(heading, pitch)
 	
 
 	if boolUpright== true and boolReloaded == true and eggCounter > 0 then
-		WTurn(center,y_axis, heading+math.rad(-90), 12)		
+		Turn(Main,z_axis,math.rad(0),12)
+		Turn(center,y_axis, heading+math.rad(-90), 12)		
+		WaitForTurns(Main,center)
 		spreadLegs( storedPitch, 5)
 		return true
 	end	
@@ -471,6 +526,7 @@ function script.FireWeapon1()
 	spawnCegAtPiece(unitID, EggCannon, "helioloadaurora", 0)
 	hideT( TableOfPieceGroups["Egg"])
 	StartThread(ReloadEggGun)
+	Turn(Main,y_axis,math.rad(zRotationMain),0.7)
 	Turn(center,y_axis,math.rad(0),0.7)
 return true
 end
