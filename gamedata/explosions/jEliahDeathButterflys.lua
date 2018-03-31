a27f632c1bcfa697e9d5a92aec9206c9643767c3
PicassoCT <PicassoCT@gmx.de>
2018-03-31 15:45:42 +0200

    Version{128.0}

---
+++ gamedata/explosions/jEliahDeathButterflys.lua
@@ -0,0 +1,108 @@
+return {
+	["jeliadeath"] = {
+		
+
+		rainbow = {
+			air = true,
+			class = [[CSimpleParticleSystem]],
+			count = 15,
+			ground = true,
+			properties = {
+				airdrag = 0.8,
+				alwaysvisible = true,
+				colormap = [[
+				0.1 0.1 0.1 0.01
+				0.9 0.1 0.1 0.05
+				0.7 0.5 0.1 0.05
+				0.1 0.8 0.9 0.05
+				0.1 0.1 0.1 0.001]],
+				directional = true,
+				emitrot = 1,
+				emitrotspread = 1,
+				emitvector = [[r0.5r-1,0.3r7,r0.5r-1]],
+				gravity = [[r0.25r-r0.25, r0.01, r0.25r-r0.25]],
+				numparticles = 1,
+				particlelife = 50,
+				particlelifespread = 150,
+				particlesize = 6,
+				particlesizespread = 12,
+				particlespeed = 10,
+				particlespeedspread = 5,
+				pos = [[0, 2, 0]],
+				sizegrowth = [[0.0 0.0000000000000000001]],
+				sizemod = 0.99999999,
+				texture = [[jeliahbutterfly]],
+				useairlos = false,
+			},
+		},
+		rainbow2 = {
+			air = true,
+			class = [[CSimpleParticleSystem]],
+			count = 15,
+			ground = true,
+			properties = {
+				airdrag = 0.8,
+				alwaysvisible = true,
+				colormap = [[
+				0.1 0.1 0.1 0.01
+				0.1 0.1 0.9 0.05
+				0.5 0.3 0.5 0.05
+				0.9 0.1 0.1 0.05
+				0.1 0.1 0.1 0.001]],
+				directional = true,
+				emitrot = 1,
+				emitrotspread = 1,
+				emitvector = [[r0.5r-1,0.3r7,r0.5r-1]],
+				gravity = [[r0.25r-r0.25, r0.01, r0.25r-r0.25]],
+				numparticles = 1,
+				particlelife = 50,
+				particlelifespread = 150,
+				particlesize = 6,
+				particlesizespread = 12,
+				particlespeed = 10,
+				particlespeedspread = 5,
+				pos = [[0, 2, 0]],
+				sizegrowth = [[0.0 0.0000000000000000001]],
+				sizemod = 0.99999999,
+				texture = [[jeliahbutterfly]],
+				useairlos = false,
+			},
+		},	
+		rainbow3 = {
+			air = true,
+			class = [[CSimpleParticleSystem]],
+			count = 15,
+			ground = true,
+			properties = {
+				airdrag = 0.8,
+				alwaysvisible = true,
+				colormap = [[
+				0.1 0.1 0.1 0.01
+				0.1 0.6 0.9 0.05
+				0.7 0.5 0.1 0.05
+				0.9 0.1 0.1 0.05
+				0.1 0.1 0.1 0.001]],
+				directional = true,
+				emitrot = 1,
+				emitrotspread = 1,
+				emitvector = [[r0.5r-1,0.3r7,r0.5r-1]],
+				gravity = [[r0.25r-r0.25, r0.01, r0.25r-r0.25]],
+				numparticles = 1,
+				particlelife = 50,
+				particlelifespread = 150,
+				particlesize = 6,
+				particlesizespread = 12,
+				particlespeed = 10,
+				particlespeedspread = 5,
+				pos = [[0, 2, 0]],
+				sizegrowth = [[0.0 0.0000000000000000001]],
+				sizemod = 0.99999999,
+				texture = [[jeliahbutterfly]],
+				useairlos = false,
+			},
+		},
+		
+		
+		
+	}		
+}
