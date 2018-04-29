28bbfb39e80ea2e8956b56fe0acc7bd3944c548a
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2018-04-29 16:29:40 +0200

    added cawilduniverseappearscorono

---
+++ gamedata/explosions/cawilduniversecorona.lua
@@ -0,0 +1,87 @@
+-- trail_ar2
+
+return {
+  ["cawilduniversecorona"] = {
+  
+				sideleft = {
+								  air                = true,
+								  class              = [[CBitmapMuzzleFlame]],
+								  count              = 1,
+								  ground             = true,
+								  underwater         = 1,
+								  water              = true,
+								  properties = {
+									colormap           = [[1 0.55 0.9 0.005	  1 0.55 0.9 0.004	1 0.3 0.55 0.003  1 0.3 0.55 0.001	]],
+									dir                = [[0.5,0.5,0.5]],
+									frontoffset        = 0,
+									fronttexture       = [[nuke]],
+									length             = 0,
+									sidetexture        = [[]],
+									size               = 50,
+									sizegrowth         = 0.01,
+									ttl                = 30*15,
+								  },
+								},		
+
+				sideright = {
+								  air                = true,
+								  class              = [[CBitmapMuzzleFlame]],
+								  count              = 1,
+								  ground             = true,
+								  underwater         = 1,
+								  water              = true,
+								  properties = {
+									colormap           = [[1 0.55 0.9 0.005	  1 0.55 0.9 0.004	1 0.3 0.55 0.003  1 0.3 0.55 0.001	]],
+									dir                = [[0.5,0.5,-0.5]],
+									frontoffset        = 0,
+									fronttexture       = [[nuke]],
+									length             = 0,
+									sidetexture        = [[]],
+									size               = 50,
+									sizegrowth         = 0.01,
+									ttl                = 30*15,
+								  },
+								},			
+				sideback	= {
+								  air                = true,
+								  class              = [[CBitmapMuzzleFlame]],
+								  count              = 1,
+								  ground             = true,
+								  underwater         = 1,
+								  water              = true,
+								  properties = {
+									colormap           = [[1 0.55 0.9 0.005	  1 0.55 0.9 0.004	1 0.3 0.55 0.003  1 0.3 0.55 0.001	]],
+									dir                = [[0,0.5,0]],
+									frontoffset        = 0,
+									fronttexture       = [[nuke]],
+									length             = 0,
+									sidetexture        = [[]],
+									size               = 50,
+									sizegrowth         = 0.01,
+									ttl                = 30*15,
+								  },
+								},		
+					topdown	= {
+								  air                = true,
+								  class              = [[CBitmapMuzzleFlame]],
+								  count              = 1,
+								  ground             = true,
+								  underwater         = 1,
+								  water              = true,
+								  properties = {
+									colormap           = [[1 0.55 0.9 0.005	  1 0.55 0.9 0.004	1 0.3 0.55 0.003  1 0.3 0.55 0.001	]],
+									dir                = [[0,1 ,0]],
+									frontoffset        = 0,
+									fronttexture       = [[nuke]],
+									length             = 0,
+									sidetexture        = [[]],
+									size               = 50,
+									sizegrowth         = 0.01,
+									ttl                = 30*15,
+								  },
+								},	
+  
+  
+  },
+
+}
