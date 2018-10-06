0271aa22c177283828ae4590db36e2631a9e9dc9
PicassoCT <PicassoCT@gmx.de>
2018-10-07 01:20:53 +0200

    Update scumslum.lua
    Cannonized name

--- units/centrail/scumslum.lua
+++ units/centrail/scumslum.lua
@@ -1,7 +1,7 @@
 local unitName = "scumslum"
 
 local unitDef = {
-  name               = "Scumslum",
+  name               = "Highrise Securityhotspot",
   Description = " adds Securitybudget to keep the new citizens in line <Security Production>",
   objectName         = "scumslum.s3o",
   	buildPic = "scumslum.dds",
