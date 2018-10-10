395394281c751fa1060b098ab5f809f5ca496d36
PicassoCT <PicassoCT@gmx.de>
2018-10-10 23:48:36 +0200

    VERSION{140.0} Updated to remove Ability Button Caption lag

--- scripts/lib_UnitScript.lua
+++ scripts/lib_UnitScript.lua
@@ -33,7 +33,7 @@ along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA 02110-1301, USA.
 
-]] --
+]] -- 
 -------------- DEBUG HEADER
 -- Central Debug Header Controlled in UnitScript
 -------------- DEBUG HEADER
