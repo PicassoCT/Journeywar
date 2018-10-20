ee1196c583991587295d1021d9880bac21bc1748
PicassoCT <PicassoCT@gmx.de>
2018-10-21 00:03:48 +0200

    Work on the grey goo continued

--- scripts/gGreyGooScript.lua
+++ scripts/gGreyGooScript.lua
@@ -6,13 +6,19 @@ include "lib_Build.lua"
 
 TablesOfPiecesGroups = {}
 
+damgedInFrame=nil
 function script.HitByWeapon(x, z, weaponDefID, damage)
+damgedInFrame= Spring.GetGameFrame()
 end
 
+aimpiece = piece"aimpiece"
 
 function script.Create()
     generatepiecesTableAndArrayCode(unitID)
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+	 hideAll(unitID)
+	 showT(TablesOfPiecesGroups["NGon"],1,27)
+	 showT(TablesOfPiecesGroups["Bubble"])
 end
 
 function script.Killed(recentDamage, _)
@@ -61,6 +67,19 @@ end
 function script.Deactivate()
 
     return 0
+end
+
+function script.Killed(recentDamage)
+	if not damgedInFrame or recentDamage <= 0 or Spring.GetGameFrame() < damgedInFrame - 15 then
+		vannishAnimation()
+	else
+		deathAnimation()
+	end
+end
+
+function vannishAnimation()
+end
+function deathAnimation()
 end
 
 function script.QueryBuildInfo()
