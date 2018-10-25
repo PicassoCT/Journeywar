aa43cd56452edca4395e4bdc2de4c75181214c80
PicassoCT <PicassoCT@gmx.de>
2018-10-25 23:00:43 +0200

    Rewrote the Tutorial storyline

--- LuaRules/Gadgets/jw_MentorAI.lua
+++ LuaRules/Gadgets/jw_MentorAI.lua
@@ -2,11 +2,11 @@
 
 function gadget:GetInfo()
     return {
-        name = "Mentor AI:  Tutorial",
-        desc = "Loads and runs a Tutorial",
-        author = "Picassora",
-        date = "around since last tuesday",
-        license = "Licensed under the Recursive License: "..
+        name ="Mentor AI:  Tutorial",
+        desc ="Loads and runs a Tutorial",
+        author ="Picassora",
+        date ="around since last tuesday",
+        license ="Licensed under the Recursive License:"..
 		"a) Every version drafted, has a completely diffrent license then all its predecessors"..
 		"b) Include all previous licenses"..
 		"c) Fullfills all conditions of the previous licenses",
@@ -20,14 +20,65 @@ if (gadgetHandler:IsSyncedCode()) then
     VFS.Include("scripts/lib_jw.lua")
 
     gaiaTeam = Spring.GetGaiaTeamID()
-    tutorialMapName = "Ravenholm"
-    startscript = "TODO get startscript from singleplayerf	"
+    tutorialMapName ="Ravenholm"
+	function getStartScript(playerName, tutorialMapName)
+    return "[game]"..
+					"{"..
+					"[allyteam1]"..
+					"{"..
+					"numallies=0;"..
+					"}"..
+					"[team1]"..
+					"{"..
+					"teamleader=0;"..
+					"allyteam=1;"..
+					"}"..
+					"[ai0]"..
+					"{"..
+					"shortname=Mentor AI: Tutorial;"..
+					"name=AI: Mentor AI: Tutorial;"..
+					"team=1;"..
+					"host=0;"..
+					"}"..
+					"[modoptions]"..
+					"{"..
+					"maxspeed=20;"..
+					"}"..
+					"[allyteam0]"..
+					"{"..
+					"numallies=0;"..
+					"}"..
+					"[team0]"..
+					"{"..
+					"teamleader=0;"..
+					"allyteam=0;"..
+					"}"..
+					"[player0]"..
+					"{"..
+					"team=0;"..
+					"name="..playerName"..;"..
+					"}"..
+					"mapname="..tutorialMapName..";"..
+					"myplayername="..playerName..";"..
+					"ishost=1;"..
+					"gametype=Journeywar $VERSION;"..
+					"nohelperais=0;"..
+					"}"
+	end				
 
     function gadget:Initialize()
 		if not Game.mapName == tutorialMapName then
-			Spring.Reload (startscript)
-		end
-	
+			playerList = Spring.GetPlayerList()
+			for playerID, _ in pairs(playerList) do
+				name,active,spectator,teamID,allyTeamID,pingTime,cpuUsage,country,rank	= 	Spring.GetPlayerInfo(playerID)	
+				teamID, leader,  isDead,  isAiTeam,  side, allyTeam,  customTeamKeys, incomeMultiplier = Spring.GetTeamInfo(teamID)
+			
+				if not isDead and not isAiTeam and name ~= "" and not spectator and rank < 250 then
+					Spring.Reload (	getStartScript(name, tutorialMapName))
+					return
+				end
+			end		
+		end	
     end
 	
 	persPack= {}
