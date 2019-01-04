3c3d91466e6fcf184872ed8a8c51a7ab632b9366
PicassoCT <PicassoCT@gmx.de>
2019-01-04 14:57:06 +0100

    What the heck did i do with the gamagardener?

--- units/centrail/cgamagardener.lua
+++ units/centrail/cgamagardener.lua
@@ -1,69 +1,69 @@
 local unitName = "cgamagardener"
 
 local unitDef = {
-name = "Gammagardener",
-Description = "Landscape gammasoaking sterilization Unit <Anti Energy Production Unit>",
-objectName = "gama.s3o",
-script = "cgamagardener.lua",
-buildPic = "cgamagardener.png",
---cost
-buildCostMetal = 550,
-buildCostEnergy = 250,
-buildTime = 5,
---Health
-maxDamage = 950,
-mass=1024,
-idleAutoHeal =1,
---Movement
-Acceleration = 0.4,
-BrakeRate = 0.3,
-FootprintX = 3,
-FootprintZ = 3,
-MaxSlope = 60,
-MaxVelocity = 4.5,
-MaxWaterDepth = 20,
-MovementClass = "Default2x2",
-TurnRate = 900,
-onoffable=true,
-activatewhenbuilt=false,
-sightDistance = 500,
-nanocolor=[[0 0.9 0.9]],
-
+	name = "Gammagardener",
+	Description = "Landscape gammasoaking sterilization Unit <Anti Energy Production Unit>",
+	objectName = "gama.s3o",
+	script = "cgamagardener.lua",
+	buildPic = "cgamagardener.png",
+	--cost
+	buildCostMetal = 550,
+	buildCostEnergy = 250,
+	buildTime = 5,
+	--Health
+	maxDamage = 950,
+	mass=1024,
+	idleAutoHeal =1,
+	--Movement
+	Acceleration = 0.4,
+	BrakeRate = 0.3,
+	FootprintX = 3,
+	FootprintZ = 3,
+	MaxSlope = 60,
+	MaxVelocity = 4.5,
+	MaxWaterDepth = 20,
+	MovementClass = "Default2x2",
+	TurnRate = 900,
+	onoffable=true,
+	activatewhenbuilt=false,
+	sightDistance = 500,
+	nanocolor=[[0 0.9 0.9]],
+	
 	explodeAs="citadelldrone",
 	selfDestructAs="citadelldrone", 
-
-CanAttack = false,
-CanGuard = true,
-
-CanMove = true,
-CanPatrol = true,
-CanStop = true,
-LeaveTracks = true, 
+	
+	CanAttack = false,
+	CanGuard = true,
+	
+	CanMove = true,
+	CanPatrol = true,
+	CanStop = true,
+	LeaveTracks = true, 
 	trackType ="BIGFoot",
-	 trackStrength=4,
+	trackStrength=4,
 	trackWidth =16,
 	trackOffset =0,
-
-	  customParams = {},
- sfxtypes = {
-    explosiongenerators = {
-	   "custom:gamagarden",
-	   "custom:gamagarden",
 	
-
-	   
-						  },
-	
-				},
-				
+	customParams = {},
+	sfxtypes = {
+		explosiongenerators = {
+			"custom:gamagarden",
+			"custom:gamagarden",
+			
+			
+			
+		},
 		
+	},
 	
 	
-Category = [[LAND]],
-
-
 	
-
+	
+	Category = [[LAND]],
+	
+	
+	
+	
 }
 
 return lowerkeys({ [unitName] = unitDef })
