35d8bf11102be32011179713ae679cd4dea06556
Florian Seidl -Schulz <PicassoCT@gmx.de>
2019-03-26 01:11:06 +0100

    new version of default menue  hiding widget --hides default icons

--- LuaUI/widgets/gui_hideDefaultGui.lua
+++ LuaUI/widgets/gui_hideDefaultGui.lua
@@ -1,44 +1,43 @@
 function widget:GetInfo()
   return {
-    name      = "Hide default interface",
+    name      = "Hides the default interface (classic handler version)",
     desc      = "",
     author    = "gajop, Bluestone",
     date      = "",
     license   = "GPL-v2",
     layer     = math.huge;
     enabled   = true,
-    
-    hidden    = false, -- don't show in the widget selector
+   
+    hidden    = true; -- don't show in the widget selector
     api       = true; -- load before all others?
   }
 end
-
+ 
 function widget:Initialize()
-  widgetHandler:RegisterGlobal("LayoutButtons", widgetHandler)
-
-  -- Spring.SendCommands("hideinterface  1")
-  --Spring.SendCommands("Console 0")
-
-  -- Spring.SendCommands("Console 0")  
-  -- Spring.SendCommands("ResBar 0")
+  --widgetHandler:RegisterGlobal("LayoutButtons", DummyLayoutHandler)
+  widgetHandler:ConfigLayoutHandler(DummyLayoutHandler)
+  Spring.ForceLayoutUpdate()
+ 
+  Spring.SendCommands("Console 0")
+  Spring.SendCommands("ResBar 0")
   Spring.SendCommands("ToolTip 0")
   Spring.SendCommands("Clock 0")
   Spring.SendCommands("Info 0")
   Spring.SendCommands("Fps 0")
-  
- -- gl.SlaveMiniMap(true)
- -- gl.ConfigMiniMap(-1,-1,-1,-1)
+ 
+  gl.SlaveMiniMap(true)
+  gl.ConfigMiniMap(-1,-1,-1,-1)
 end
-
+ 
 local function DummyLayoutHandler(xIcons, yIcons, cmdCount, commands)
-  handler.commands   = commands
-  handler.commands.n = cmdCount
-  handler:CommandsChanged()
+  widhetHandler.commands   = commands
+  widhetHandler.commands.n = cmdCount
+  widhetHandler:CommandsChanged()
   return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, {}
 end
-
+ 
 function widget:GameSetup()
   -- sets status instantly to ready & hides the pre-game UI
   -- you might want/need to change this part & integrate it into your spawning gadget!
-  return true, true 
+  return true, true
 end
